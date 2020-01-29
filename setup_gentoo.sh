f28ce220d6e2b84aa6297dd5b8c4a461b2d29c3a
kenny <Mentaloutlaw@gmail.com>
2020-01-28 21:18:12 -0500

    re add download stage3

--- setup_gentoo.sh
+++ setup_gentoo.sh
@@ -1,21 +1,27 @@
 #backs up the default make.conf
 #this puts some things in place like your make.conf, aswell as package.use
 
-LIGHTGREEN='\033[1;32m'
-#this block of code was added from post_chroot.sh, START
-LIGHTBLUE='\033[1;34m'
-printf ${LIGHTBLUE}"Enter the disk name you want to install gentoo on (ex, sda)\n>"
-read disk
-disk="${disk,,}"
-printf ${LIGHTBLUE}"Enter the username for your NON ROOT user\n>"
-#There is a possibility this won't work since the handbook creates a user after rebooting and logging as root
-read username
-username="${username,,}"
-printf ${LIGHTBLUE}"Enter Yes to make a kernel from scratch, edit to edit the hardened config, or No to use the default hardened config\n>"
-read kernelanswer
-kernelanswer="${kernelanswer,,}"
-printf ${LIGHTBLUE}"Enter the Hostname you want to use\n>"
-read hostname
+#LIGHTGREEN='\033[1;32m'
+##this block of code was added from post_chroot.sh, START
+#LIGHTBLUE='\033[1;34m'
+#printf ${LIGHTBLUE}"Enter the disk name you want to install gentoo on (ex, sda)\n>"
+#read disk
+#disk="${disk,,}"
+#printf ${LIGHTBLUE}"Enter the username for your NON ROOT user\n>"
+##There is a possibility this won't work since the handbook creates a user after rebooting and logging as root
+#read username
+#username="${username,,}"
+#printf ${LIGHTBLUE}"Enter Yes to make a kernel from scratch, edit to edit the hardened config, or No to use the default hardened config\n>"
+#read kernelanswer
+#kernelanswer="${kernelanswer,,}"
+#printf ${LIGHTBLUE}"Enter the Hostname you want to use\n>"
+#read hostname
+
+GENTOO_TYPE=latest-install-amd64-minimal
+STAGE3_PATH_URL=http://distfiles.gentoo.org/releases/amd64/autobuilds/$GENTOO_TYPE.txt
+STAGE3_PATH=$(curl -s $STAGE3_PATH_URL | grep -v "^#" | cut -d" " -f1)
+STAGE3_URL=http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3_PATH
+wget $STAGE3_URL
 #this block of code was added from post_chroot.sh, END
 
 cd /mnt/gentoo/
@@ -69,120 +75,121 @@ mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
 
 cd /mnt/gentoo
+printf "now run post_chroot.sh\n"
 #TODO this part of the script doesn't accept user input, you must have all read commands before you change root
-chroot /mnt/gentoo /bin/bash << "EOT"
-
-#TODO
-#Replace all the echo with printf
-#env-update
-#source /etc/profile
-#export PS1="(chroot) ${PS1}"
-cd deploygentoo-master
-scriptdir=$(pwd)
-cd ..
-
-#mount /dev/sda1 /boot
-part_1=("/dev/${disk}1")
-part_2=("/dev/${disk}2")
-dev_sd=("/dev/$disk")
-mount $part_1
-printf "mounted boot\n"
-emerge-webrsync
-printf "webrsync complete\n"
-
-printf "preparing to do big emerge\n"
-
-emerge --verbose --update --deep --newuse @world
-printf "big emerge complete\n"
-printf "America/New_York\n" > /etc/timezone
-emerge --config sys-libs/timezone-data
-printf "timezone data emerged\n"
-en_US.UTF-8 UTF-8
-printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen
-locale-gen
-printf "script complete\n"
-eselect locale set 4
-env-update && source /etc/profile
-
-#Installs the kernel
-printf "preparing to emerge kernel sources\n"
-emerge sys-kernel/gentoo-sources
-#emerge sys-kernel/ck-sources-5.4.7
-#sleep5
-#emerge sys-kernel/ck-sources
-cd /usr/src/linux
-emerge sys-apps/pciutils
-emerge lzop
-emerge app-arch/lz4
-printf "Do you want to configure your own kernel?\n"
-if [ $kernelanswer = "no" ]; then
-	cp /deploygentoo-master/gentoo/kernel/gentoohardenedminimal /usr/src/linux
-	mv gentoohardenedminimal .config
-	make olddefconfig
-	make && make modules_install
-	make install
-	printf "Kernel installed\n"
-elif [ $kernelanswer = "edit" ]; then
-	cp /deploygentoo-master/gentoo/kernel/gentoohardenedminimal /usr/src/linux
-	mv gentoohardenedminimal .config
-	make menuconfig
-	make && make modules_install
-	make install
-	printf "Kernel installed\n"
-else
-	printf "time to configure your own kernel\n"
-	make menuconfig
-	make && make modules_installl
-	make install
-	printf "Kernel installed\n"
-fi
-
-#enables DHCP
-sed -i -e "s/localhost/$hostname/g" /etc/conf.d/hostname
-emerge --noreplace net-misc/netifrc
-printf "config_enp0s3=\"dhcp\"\n" >> /etc/conf.d/net
-#printf "/dev/sda1\t\t/boot\t\text4\t\tdefaults,noatime\t0 2\n" >> /etc/fstab
-printf "%s\t\t/boot\t\text4\t\tdefaults,noatime\t0 2\n" $part_1 >> /etc/fstab
-#printf "/dev/sda2\t\t/\t\text4\t\tnoatime\t0 1\n" >> /etc/fstab
-printf "%s\t\t/\t\text4\t\tnoatime\t0 1\n" $part_2 >> /etc/fstab
-cd /etc/init.d
-ln -s net.lo net.enp0s3
-rc-update add net.enp0s3 default
-printf "dhcp enabled\n"
-emerge app-admin/sysklogd
-emerge app-admin/sudo
-printf "just emerged sudo\n"
-rm -rf /etc/sudoers
-cd $scriptdir
-cp sudoers /etc/
-printf "installed sudo and enabled it for wheel group\n"
-rc-update add sysklogd default
-emerge sys-apps/mlocate
-emerge net-misc/dhcpcd
-
-#installs grub
-emerge --verbose sys-boot/grub:2
-#grub-install /dev/sda
-grub-install $dev_sd
-grub-mkconfig -o /boot/grub/grub.cfg
-printf "updated grub\n"
-useradd -m -G users,wheel,audio -s /bin/bash $username
-printf "just tried to add our user\n"
-cd ..
-printf "cleaning up\n"
-mv deploygentoo-master.zip /home/$username
-rm -rf /deploygentoo-master
-stage3=$(ls stage3*)
-rm -rf $stage3
-printf "preparing to exit the system, run the following commands and then reboot without the CD\n"
-printf "you should now have a working Gentoo installation, dont forget to set your root and user passwords!\n"
-printf ${LIGHTGREEN}"passwd\n"
-printf ${LIGHTGREEN}"passwd %s\n" $username
-printf ${LIGHTGREEN}"exit\n"
-printf ${LIGHTGREEN}"cd\n"
-printf ${LIGHTGREEN}"umount -l /mnt/gentoo/dev{/shm,/pts,}\n"
-printf ${LIGHTGREEN}"umount -R /mnt/gentoo\n"
-printf ${LIGHTGREEN}"reboot\n"
-rm -rf /post_chroot.sh
-#exit
-EOT
+#chroot /mnt/gentoo /bin/bash << "EOT"
+#
+##TODO
+##Replace all the echo with printf
+##env-update
+##source /etc/profile
+##export PS1="(chroot) ${PS1}"
+#cd deploygentoo-master
+#scriptdir=$(pwd)
+#cd ..
+#
+##mount /dev/sda1 /boot
+#part_1=("/dev/${disk}1")
+#part_2=("/dev/${disk}2")
+#dev_sd=("/dev/$disk")
+#mount $part_1
+#printf "mounted boot\n"
+#emerge-webrsync
+#printf "webrsync complete\n"
+#
+#printf "preparing to do big emerge\n"
+#
+#emerge --verbose --update --deep --newuse @world
+#printf "big emerge complete\n"
+#printf "America/New_York\n" > /etc/timezone
+#emerge --config sys-libs/timezone-data
+#printf "timezone data emerged\n"
+#en_US.UTF-8 UTF-8
+#printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen
+#locale-gen
+#printf "script complete\n"
+#eselect locale set 4
+#env-update && source /etc/profile
+#
+##Installs the kernel
+#printf "preparing to emerge kernel sources\n"
+#emerge sys-kernel/gentoo-sources
+##emerge sys-kernel/ck-sources-5.4.7
+##sleep5
+##emerge sys-kernel/ck-sources
+#cd /usr/src/linux
+#emerge sys-apps/pciutils
+#emerge lzop
+#emerge app-arch/lz4
+#printf "Do you want to configure your own kernel?\n"
+#if [ $kernelanswer = "no" ]; then
+#	cp /deploygentoo-master/gentoo/kernel/gentoohardenedminimal /usr/src/linux
+#	mv gentoohardenedminimal .config
+#	make olddefconfig
+#	make && make modules_install
+#	make install
+#	printf "Kernel installed\n"
+#elif [ $kernelanswer = "edit" ]; then
+#	cp /deploygentoo-master/gentoo/kernel/gentoohardenedminimal /usr/src/linux
+#	mv gentoohardenedminimal .config
+#	make menuconfig
+#	make && make modules_install
+#	make install
+#	printf "Kernel installed\n"
+#else
+#	printf "time to configure your own kernel\n"
+#	make menuconfig
+#	make && make modules_installl
+#	make install
+#	printf "Kernel installed\n"
+#fi
+#
+##enables DHCP
+#sed -i -e "s/localhost/$hostname/g" /etc/conf.d/hostname
+#emerge --noreplace net-misc/netifrc
+#printf "config_enp0s3=\"dhcp\"\n" >> /etc/conf.d/net
+##printf "/dev/sda1\t\t/boot\t\text4\t\tdefaults,noatime\t0 2\n" >> /etc/fstab
+#printf "%s\t\t/boot\t\text4\t\tdefaults,noatime\t0 2\n" $part_1 >> /etc/fstab
+##printf "/dev/sda2\t\t/\t\text4\t\tnoatime\t0 1\n" >> /etc/fstab
+#printf "%s\t\t/\t\text4\t\tnoatime\t0 1\n" $part_2 >> /etc/fstab
+#cd /etc/init.d
+#ln -s net.lo net.enp0s3
+#rc-update add net.enp0s3 default
+#printf "dhcp enabled\n"
+#emerge app-admin/sysklogd
+#emerge app-admin/sudo
+#printf "just emerged sudo\n"
+#rm -rf /etc/sudoers
+#cd $scriptdir
+#cp sudoers /etc/
+#printf "installed sudo and enabled it for wheel group\n"
+#rc-update add sysklogd default
+#emerge sys-apps/mlocate
+#emerge net-misc/dhcpcd
+#
+##installs grub
+#emerge --verbose sys-boot/grub:2
+##grub-install /dev/sda
+#grub-install $dev_sd
+#grub-mkconfig -o /boot/grub/grub.cfg
+#printf "updated grub\n"
+#useradd -m -G users,wheel,audio -s /bin/bash $username
+#printf "just tried to add our user\n"
+#cd ..
+#printf "cleaning up\n"
+#mv deploygentoo-master.zip /home/$username
+#rm -rf /deploygentoo-master
+#stage3=$(ls stage3*)
+#rm -rf $stage3
+#printf "preparing to exit the system, run the following commands and then reboot without the CD\n"
+#printf "you should now have a working Gentoo installation, dont forget to set your root and user passwords!\n"
+#printf ${LIGHTGREEN}"passwd\n"
+#printf ${LIGHTGREEN}"passwd %s\n" $username
+#printf ${LIGHTGREEN}"exit\n"
+#printf ${LIGHTGREEN}"cd\n"
+#printf ${LIGHTGREEN}"umount -l /mnt/gentoo/dev{/shm,/pts,}\n"
+#printf ${LIGHTGREEN}"umount -R /mnt/gentoo\n"
+#printf ${LIGHTGREEN}"reboot\n"
+#rm -rf /post_chroot.sh
+##exit
+#EOT
