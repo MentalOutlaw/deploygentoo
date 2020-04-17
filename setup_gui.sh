5d12de929f8ea4b096ec58aaba35730b4b738d3d
minty mental <mentaloutlaw@gmail.com>
2020-04-17 08:38:33 -0400

    added setup gui script

---
+++ setup_gui.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#TODO
+#there are some situations in which the deploygentoo-master folder will be named deploygentoo
+#make this script work with that folder name aswell
+
+check_file_exists () {
+	file=$1
+	if [ -e $file ]; then
+		exists=true
+	else
+		printf "%s doesn't exist\n" $file
+		exists=false
+		$2
+	fi
+}
+check_dir_exists () {
+	file=$1
+	if [ -d $file ]; then
+		exists=true
+	else
+		printf "%s doesn't exist\n" $file
+		exists=false
+		$2
+	fi
+}
+
+if [ "$EUID" -ne 0 ]
+  then printf "The script has to be run as root.\n"
+  exit
+fi
+
+printf "This script is designed for gentoo linux and it will not work in any other OS\n"
+printf "Installing dependencies listed in dependencies.txt...\n"
+
+DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
+SOFTWARE="`sed -e 's/#.*$//' -e '/^$/d' software.txt | tr '\n' ' '`"
+
+#Installs and configures layman
+#emerge app-portage/layman
+sed -i "s/conf_type : repos.conf/conf_type : make.conf/g" /etc/layman/layman.cfg
+echo >> "source /var/lib/layman/make.conf" /etc/portage/make.conf
+echo >> "PORTDIR_OVERLAY=\"${PORTDIR_OVERLAY} /usr/local/portage/\"" /etc/portage/make.conf
+layman -a libressl
+layman -S
+
+emerge --autounmask-write $DEPLIST
+USE="X" emerge app-editors/vim
+USE="perl xft" emerge x11-terms/rxvt-unicode
+USE="cli libmpv" emerge media-video/mpv
+
+printf "installed dependencies\n"
+script_home=$(pwd)
+check_dir_exists /apps
+if $exists; then
+	printf "apps directory already exists"
+else
+	unzip rice.zip
+fi
+cd apps/
+chmod +x rice-gentoo.sh
+sh rice-gentoo.sh
+printf "completed installing dependencies\n"
+printf "your GUI should be set up now, use startx to launch it\n"
