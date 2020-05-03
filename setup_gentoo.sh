cbbfc8660dd3e20018e50f4a4b5df5191ad504fa
minty mental <mentaloutlaw@gmail.com>
2020-05-03 14:43:18 -0400

    bug fixes

--- setup_gentoo.sh
+++ setup_gentoo.sh
@@ -19,7 +19,7 @@ while true
 do
 	if grep -Fxq "$rootpart" /root/disks
 	#printf "up here\n"
-	#printf "rootpart is %s\n" $rootpart
+	printf "rootpart is %s\n" $rootpart
 	then
 		#continue running the script
 		printf ${LIGHTGREEN}"%s is valid :D continuing with the script\n" $rootpart
