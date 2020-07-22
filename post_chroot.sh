3a5594eb5e8aed5fab6057066de3879ee106b90f
gentoolab <gentoolab>
2020-07-22 10:18:02 -0400

    test lto changes within chroot during install

--- post_chroot.sh
+++ post_chroot.sh
@@ -204,14 +204,13 @@ if [ $performance_opts = "yes" ]; then
     #this command doesn't work to emerge ltoize
     #TODO figure out how to fix it
     emerge -q sys-config/ltoize
-
     #TODO add option to append -falign-functions=32 to CFLAGS if user has an Intel Processor
-    sed -i -e 's/CFLAGS=\"${COMMON_FLAGS}\"/CFLAGS=\"-march=native ${CFLAGS} -pipe\"/g' /etc/portage/make.conf
+    sed -i 's/CFLAGS=\"${COMMON_FLAGS}\"/CFLAGS=\"-march=native ${CFLAGS} -pipe\"/g' /etc/portage/make.conf
     sed -i 's/CXXFLAGS=\"${COMMON_FLAGS}\"/CXXFLAGS=\"${CFLAGS}\"/g' /etc/portage/make.conf
     #echo "NTHREADS=\"${cpus}\"" >> /etc/portage/make.conf
     sed -i '5s/^/NTHREADS=\"${cpus}\"\n\n/' /etc/portage/make.conf
     sed -i '6s/^/source make.conf.lto\n\n/' /etc/portage/make.conf
-    sed -i '11s/^/CPU_FLAGS_X86=\"aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3\"' /etc/portage/make.conf
+    sed -i '11s/^/CPU_FLAGS_X86=\"aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3\"\n/' /etc/portage/make.conf
     emerge -e @world
     printf "performance enhancements setup, you'll have to emerge sys-config/ltoize to complete\n"
 elif [ $performance_opts = "no" ]; then
--- test.make.conf.sh
+++
@@ -1,7 +0,0 @@
-#TODO add option to append -falign-functions=32 to CFLAGS if user has an Intel Processor
-sed -i -e 's/CFLAGS=\"${COMMON_FLAGS}\"/CFLAGS=\"-march=native ${CFLAGS} -pipe\"/g' /etc/portage/make.conf
-sed -i 's/CXXFLAGS=\"${COMMON_FLAGS}\"/CXXFLAGS=\"${CFLAGS}\"/g' /etc/portage/make.conf
-#echo "NTHREADS=\"${cpus}\"" >> /etc/portage/make.conf
-sed -i '5s/^/NTHREADS=\"${cpus}\"\n\n/' /etc/portage/make.conf
-sed -i '6s/^/source make.conf.lto\n\n/' /etc/portage/make.conf
-sed -i '11s/^/CPU_FLAGS_X86=\"aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3\"' /etc/portage/make.conf
